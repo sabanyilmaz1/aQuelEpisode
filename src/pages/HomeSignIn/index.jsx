ef9bd0ba3c48d0c9ca76e44889c148d57db488a2
sabanyilmaz1 <syilmaz1>
2022-02-15 17:22:37 +0100

    Stye Page HomeSignIn

--- src/pages/HomeSignIn/index.jsx
+++ src/pages/HomeSignIn/index.jsx
@@ -1,9 +1,81 @@
-function Home() {
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+//Logo
+import logo from '../../assets/logoHome.svg'
+
+//Style
+import { LogoWrapper } from './style'
+import { DescriptionBloc } from './style'
+import { DescriptionSpan } from './style'
+import { ConnexionSpan } from './style'
+import { ConnexionBloc } from './style'
+import { TextInput } from './style'
+import { SuivantBtn } from './style'
+import { SuivantSpan } from './style'
+
+function HomeSignIn() {
+  // Afficher un message d'alerte en cas de problème de saisi
+  const [validation, setValidation] = useState('')
   return (
     <div>
-      <h1> Page d'accueil de connexion</h1>
+      <LogoWrapper>
+        <img src={logo} alt="Logo" />
+      </LogoWrapper>
+
+      <DescriptionBloc>
+        <DescriptionSpan>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          scelerisque arcu eget arcu mollis, at consectetur dui malesuada. Sed a
+          magna fringilla, convallis ex nec, rhoncus diam. Suspendisse porttitor
+          imperdiet dui, a sodales dolor tincidunt maximus. Maecenas eu lorem
+          consectetur est lacinia dapibus id faucibus nibh. In feugiat orci
+          eros, nec sodales dui rhoncus eu. Nullam eget elit in enim fermentum
+          volutpat at eget ante. Cras placerat eros id dolor accumsan, a dapibus
+          eros porta. Ut ante erat, luctus malesuada euismod ac, maximus vel
+          libero.
+        </DescriptionSpan>
+      </DescriptionBloc>
+
+      <ConnexionBloc>
+        <ConnexionSpan>Connexion</ConnexionSpan>
+
+        <div>
+          <form>
+            <div>
+              <label htmlFor="SignInEmail"></label>
+              <TextInput
+                id="SignInEmail"
+                name="email"
+                type="email"
+                placeholder="Votre email ..."
+                required
+              />
+            </div>
+
+            <div>
+              <label htmlFor="PwdEmail"></label>
+              <TextInput
+                id="PwdEmail"
+                name="pwd"
+                type="password"
+                placeholder="Votre mot de passe ..."
+                required
+              />
+            </div>
+            <p>{validation}</p>
+
+            <SuivantBtn>
+              <SuivantSpan>Suivant</SuivantSpan>
+            </SuivantBtn>
+          </form>
+          <button>
+            <Link to="/inscription">CRÉER UN COMPTE</Link>
+          </button>
+          <button>Mot de passe oublié ?</button>
+        </div>
+      </ConnexionBloc>
     </div>
   )
 }
 
-export default Home
+export default HomeSignIn
---
+++ src/pages/HomeSignIn/style.jsx
@@ -0,0 +1,75 @@
+import styled from 'styled-components'
+
+export const LogoWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-item: center;
+  margin-bottom: 10px;
+`
+
+export const DescriptionBloc = styled.div`
+  width: auto;
+  height: auto;
+  left: 29px;
+  top: 192px;
+  background: #758bfd;
+  border-radius: 25px;
+  text-align: left;
+  padding: 10px;
+`
+export const DescriptionSpan = styled.span`
+  font-family: 'Montserrat', sans-serif;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 29px;
+  letter-spacing: -0.005em;
+  color: #f1f2f6;
+`
+
+export const ConnexionSpan = styled.span`
+  font-family: 'Bebas Neue', sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 48px;
+  line-height: 58px;
+  color: #f1f2f6;
+`
+
+export const ConnexionBloc = styled.div`
+  width: 731px;
+  height: 535px;
+  background: #758bfd;
+  border-radius: 25px;
+  margin-top: 50px;
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+`
+
+export const TextInput = styled.input`
+  width: 600px;
+  height: 40px;
+  background: #fcfcfc;
+  border-radius: 20px;
+  margin-top: 50px;
+`
+
+export const SuivantBtn = styled.button`
+  margin-top: 75px;
+  background: #ff8600;
+  border-radius: 30px;
+  margin: 75px;
+  width: 200px;
+`
+export const SuivantSpan = styled.span`
+  font-family: 'Bebas Neue', sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 35px;
+  line-height: 58px;
+  color: #f1f2f6;
+`
